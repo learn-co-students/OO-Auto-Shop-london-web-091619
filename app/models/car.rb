a1f6bb4c617be643a58c01172076ee98e11f6d43
Tim Golding <tgolding55@hotmail.com>
2019-09-23 08:34:23 +0100

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,26 @@
 class Car
+@@all = []
+  attr_reader :make, :model, :classification, :owner, :mechanic
 
-  attr_reader :make, :model
-
-  def initialize(make, model, classification)
+  def initialize(make, model, classification, owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @mechanic = mechanic
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.classifications
+    self.all.map{|car| car.classification}
+  end
+
+  def find_mechanic
+    Mechanic.all.select{|mechanic| mechanic.specialty == self.classification}
   end
 
 end
--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,22 @@
 class CarOwner
-
+@@all = []
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def owned_cars
+    Car.all.select{|car| car.owner == self}
+  end
+
+  def mechanic_list
+    owned_cars.map{|car| car.mechanic}
+  end
+
+  def self.average_cars
+    Car.all.length.to_f / self.all.length
+  end
 
 end
